23f56bea1314090a13af7a8b15c58d67af106c10
Joe Steven <stevenjoe1312@gmail.com>
2024-02-08 20:54:34 +0700

    1 Hour - Continuing Info

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,10 +1,20 @@
 import imageComputer from "/images/image-computer.png";
 import imageDevices from "/images/image-devices.png";
 
+import blacklist from "/images/icon-blacklist.svg";
+import text from "/images/icon-text.svg";
+import preview from "/images/icon-preview.svg";
+
+import google from "/images/logo-google.png";
+import ibm from "/images/logo-ibm.png";
+import microsoft from "/images/logo-microsoft.png";
+import hp from "/images/logo-hp.png";
+import vectorGraphics from "/images/logo-vector-graphics.png";
+
 function Info() {
     return (
-        <div className="flex flex-col items-center text-center gap-10">
-            <div className="flex flex-col gap-10">
+        <div className="flex flex-col items-center text-center gap-20 md:gap-10">
+            <div className="flex flex-col gap-10 px-10 md:px-0">
                 <h2 className="font-bold text-3xl">
                     Keep track of your snippets
                 </h2>
@@ -16,11 +26,11 @@ function Info() {
                 </p>
             </div>
 
-            <div className="flex gap-32 -translate-x-64">
-                <div>
-                    <img src={imageComputer} />
+            <div className="flex flex-col md:flex-row gap-10 md:gap-32 md:-translate-x-64 px-10 md:px-0">
+                <div className="flex flex-col items-center justify-center">
+                    <img src={imageComputer} className="w-[30rem] md:w-auto" />
                 </div>
-                <div className="flex flex-col gap-20 justify-center text-left">
+                <div className="flex flex-col gap-10 md:gap-20 justify-center text-center md:text-left">
                     <div className="flex flex-col gap-5">
                         <h3 className="font-bold text-2xl">Quick Search</h3>
                         <p className="text-grayish_blue">
@@ -44,7 +54,7 @@ function Info() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-10 px-10 md:px-0">
                 <h2 className="font-bold text-3xl">
                     Access Clipboard anywhere
                 </h2>
@@ -54,6 +64,52 @@ function Info() {
                 </p>
                 <img src={imageDevices} />
             </div>
+
+            <div className="flex flex-col items-center text-center gap-20 px-10 md:px-0">
+                <div className="flex flex-col gap-10">
+                    <h3 className="font-bold text-3xl">
+                        Supercharge your workflow
+                    </h3>
+                    <p className="text-grayish_blue">
+                        We’ve got the tools to boost your productivity.
+                    </p>
+                </div>
+                <div className="flex flex-col md:flex-row items-center justify-center gap-20">
+                    <div className="flex flex-col gap-10 items-center justify-center text-center">
+                        <img src={blacklist} className="w-14" />
+                        <h4 className="font-bold text-2xl">
+                            Create blacklists
+                        </h4>
+                        <p className="text-grayish_blue">
+                            Ensure sensitive information never makes its way to
+                            <br />
+                            your clipboard by excluding certain sources.
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-10 items-center justify-center text-center">
+                        <img src={text} className="w-14" />
+                        <h4 className="font-bold text-2xl">
+                            Plain text snippets
+                        </h4>
+                        <p className="text-grayish_blue">
+                            Remove unwanted formatting from copied text for a
+                            <br />
+                            consistent look.
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-10 items-center justify-center text-center">
+                        <img src={preview} className="w-14" />
+                        <h4 className="font-bold text-2xl">Sneak preview</h4>
+                        <p className="text-grayish_blue">
+                            Quick preview of all snippets on your Clipboard for
+                            <br />
+                            easy access.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div></div>
         </div>
     );
 }
